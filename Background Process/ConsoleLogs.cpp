de526433b67ab892cda39ed1b20d008aecb335e8
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-04-08 15:45:18 +0800

    Added  codes

--- Background Process/ConsoleLogs.cpp
+++ Background Process/ConsoleLogs.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <Windows.h>
+#include <thread>
 
+using namespace std::literals::chrono_literals;
 
 int main() {
 
-	std::cout << "Hello World" << std::endl;
+	while (true) {
+		std::this_thread::sleep_for(10ms);
+		std::ifstream ReadLogs("Logs");
+			std::string Log;
+			ReadLogs >> Log;
+			std::cout << "Key pressed : " << Log << "\n";
+		ReadLogs.close();
+		system("cls");
+	}
+	
 
 	return EXIT_SUCCESS;
 }
